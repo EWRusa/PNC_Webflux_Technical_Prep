3e3e0d4a486e1ff95a46dcd47e6f1280da844b4f
EWRusa <stringcheeseinc1@gmail.com>
2026-08-16 18:16:17 -0600

    back end basics and compose

---
+++ demo-flux/demo-flux/src/main/java/com/emmett/demo_flux/DemoFluxApplication.java
@@ -0,0 +1,13 @@
+package com.emmett.demo_flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFluxApplication.class, args);
+	}
+
+}
